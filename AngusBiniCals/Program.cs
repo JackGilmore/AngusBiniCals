de512c47a6fb289faffe12f603aa6c610a10ced6
Jack Gilmore <gilmorejackc@gmail.com>
2023-02-19 14:31:04 +0000

    Listen to port 9000

--- AngusBiniCals/Program.cs
+++ AngusBiniCals/Program.cs
@@ -14,6 +14,7 @@ namespace AngusBiniCals
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:9000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
